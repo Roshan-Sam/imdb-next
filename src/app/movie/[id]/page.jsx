a095a0fd6fd9ff87ae90145d4c42fe3d1659ee89
Roshan-Sam <roshansamchirathalackal9898@gmail.com>
2024-09-15 21:41:04 +0530

    add search box and functionality

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,13 +1,10 @@
 import Image from 'next/image';
 
 export default async function MoviePage({ params }) {
-
     const movieId = params.id;
-
     const res = await fetch(
         `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
     );
-
     const movie = await res.json();
 
     return (
